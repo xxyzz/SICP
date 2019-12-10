9f70a1051a3417f138855dccb68beb32bd17f046
xxyzz <gitpull@protonmail.com>
2019-12-10 21:57:19 +0800

    Exercise 1.44

---
+++ 1_Structure_and_Interpretation_of_Computer_Programs/1.3_Formulating_Abstractions_with_Higher-Order_Procedures/Exercise_1_44.rkt
@@ -0,0 +1,26 @@
+#lang racket/base
+(require racket/function) ; identity
+
+(define dx 1)
+
+(define (compose f g)
+    (lambda (x) (f (g x))))
+
+(define (square x) (* x x))
+
+(define (repeated f n)
+    (if (< n 1)
+        identity
+        (compose f (repeated f (sub1 n)))))
+
+(define (smooth f)
+    (lambda (x)
+        (/ (+ (f (- x dx)) (f x) (f (+ x dx))) 3)))
+
+(define (n-fold-smooth f n)
+    ((repeated smooth n) f))
+
+((n-fold-smooth square 1) 1)
+; 5/ 3
+((n-fold-smooth square 2) 1)
+; 21 / 9 = 7 / 3
