5a899633994394e20054d1e9e6fdcda41c7820e7
xxyzz <gitpull@protonmail.com>
2020-01-09 22:35:52 +0800

    fix exercise 2.45 grammer error

--- 2_Building_Abstractions_with_Data/2.2_Hierarchical_Data_and_the_Closure_Property/Exercise_2_45.rkt
+++ 2_Building_Abstractions_with_Data/2.2_Hierarchical_Data_and_the_Closure_Property/Exercise_2_45.rkt
@@ -1,15 +1,15 @@
 #lang racket/base
 (require sicp-pict)
 
-(define right-split (split beside below))
-(define up-split (split below beside))
-
 (define (split op-a op-b)
     (lambda (p n)
         (if (zero? n)
             p
             (let ([smaller ((split op-a op-b) p (sub1 n))])
-                (op-a p (op-b smaller smaller)))))
+                (op-a p (op-b smaller smaller))))))
+
+(define right-split (split beside below))
+(define up-split (split below beside))
 
 (paint (right-split einstein 2))
 (paint (up-split einstein 2))
