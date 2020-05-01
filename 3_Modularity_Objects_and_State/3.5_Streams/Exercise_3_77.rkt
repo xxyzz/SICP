89b191eba4e91e7626d3358788909ab167628cd4
xxyzz <gitpull@protonmail.com>
2020-05-01 23:01:53 +0800

    3.77 delayed-integrand

---
+++ 3_Modularity_Objects_and_State/3.5_Streams/Exercise_3_77.rkt
@@ -0,0 +1,24 @@
+#lang racket/base
+(require racket/stream)
+(require racket/promise)
+
+(define (integral delayed-integrand initial-value dt)
+  (stream-cons
+   initial-value
+   (let ([integrand (force delayed-integrand)])
+     (if (stream-empty? integrand)
+         empty-stream
+         (integral (delay (stream-rest integrand))
+                   (+ (* dt (stream-first integrand))
+                      initial-value)
+                   dt)))))
+   
+(define (solve f y0 dt)
+  (define y (integral (delay dy) y0 dt))
+  (define dy (stream-map f y))
+  y)
+
+(stream-ref (solve (lambda (y) y) 1 0.001) 1000)
+; 0.001 * 1000 = 1
+; y = e^t
+; 2.716923932235896
