91608890eb69d3786830268e5c7aa5c8ce3c9562
xxyzz <gitpull@protonmail.com>
2020-05-14 22:20:31 +0800

    4.4 and & or as derived expressions

--- 4_Metalinguistic_Abstraction/4.1_The_Metacircular_Evaluator/Exercise_4_4.rkt
+++ 4_Metalinguistic_Abstraction/4.1_The_Metacircular_Evaluator/Exercise_4_4.rkt
@@ -27,3 +27,29 @@
   (eval-or-exps (or-expressions exp) env))
 
 ; derived expressions:
+(define (make-if predicate consequent alternative)
+  (list 'if predicate consequent alternative))
+
+(define (and->if exp) (expand-and-expressions (and-expressions exp)))
+(define (expand-and-expressions exps)
+  (if (null? exps)
+      #t
+      (make-if (cons not (first-exp exps))
+               #f
+               (if (last-exp? exps)
+                   (first-exp exps)
+                   (expand-and-expressions (rest-exps exps))))))
+(define (eval-and exp env)
+  (eval (and->if exp) env))
+
+(define (or->if exp) (expand-or-expressions (or-expressions exp)))
+(define (expand-or-expressions exps)
+  (if (null? exps)
+      #f
+      (make-if (first-exp exps)
+               (first-exp exps)
+               (if (last-exp? exps)
+                   #f
+                   (expand-or-expressions (rest-exps exps))))))
+(define (eval-or exp env)
+  (eval (or->if exp) env))
