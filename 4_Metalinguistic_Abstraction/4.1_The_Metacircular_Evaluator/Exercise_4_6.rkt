2f4e72bb7b0c3033b508d8d89de04881ff339350
xxyzz <gitpull@protonmail.com>
2020-05-23 12:20:25 +0800

    fix 4.6 let->combination

--- 4_Metalinguistic_Abstraction/4.1_The_Metacircular_Evaluator/Exercise_4_6.rkt
+++ 4_Metalinguistic_Abstraction/4.1_The_Metacircular_Evaluator/Exercise_4_6.rkt
@@ -3,10 +3,12 @@
 (define (make-lambda parameters body)
   (cons 'lambda (cons parameters body)))
 
-(define (let->combination exp env)
-  (let ([var-list (map car (cadr exp))]
-        [exp-list (map cadr (cadr exp))]
-        [body (caddr exp)])
+(define (let->combination exp)
+  (let* ([bindings (cadr exp)]
+         [var-list (map car bindings)]
+         [exp-list (map cadr bindings)]
+         [body (cddr exp)])
     (cons (make-lambda var-list body) exp-list)))
+; see exercise 2.26 for the difference of cons and list
 
-(define (eval-let exp env) (eval (eval-let exp env) env))
+(define (eval-let exp env) (eval (let->combination exp) env))
