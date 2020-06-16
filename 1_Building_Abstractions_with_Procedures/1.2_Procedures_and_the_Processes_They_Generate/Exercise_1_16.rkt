7d638c74e0220028edfbb71e6f521b3c33e1d137
xxyzz <gitpull@protonmail.com>
2020-06-16 22:47:39 +0800

    check for 0 exponent to avoid infinite loop
    The algorithm still holds. Prerequisites, aha.

--- 1_Building_Abstractions_with_Procedures/1.2_Procedures_and_the_Processes_They_Generate/Exercise_1_16.rkt
+++ 1_Building_Abstractions_with_Procedures/1.2_Procedures_and_the_Processes_They_Generate/Exercise_1_16.rkt
@@ -1,25 +1,33 @@
-#lang racket
+#lang racket/base
 
-; Right-to-left binary method for exponentiation
-; from section 4.6.3 (page 462) of Seminumerical Algorithms.
-; Volume 2 of The Art of Computer Programming. 2nd edition.
-;
-; A1. [Initialize.] Set N ← n, Y ← 1, Z ← x.
-; A2. [Halve N.] (At this point, x^n = Y * Z^N.) Set t ← N mod 2 and N ← ⌊N/2⌋.
-;   If t = 0, skip to step A5.
-; A3. [Multiply Y by Z.] Set Y ← Z times Y.
-; A4. [N = 0?] If N = 0, the algorithm terminates, with Y as the answer.
-; A5. [Square Z.] Set Z ← Z times Z, and return to step A2.
-;
-; Racket manual: https://docs.racket-lang.org/reference/generic-numbers.html
+;; Right-to-left binary method for exponentiation
+;; from section 4.6.3 (page 462) of Seminumerical Algorithms.
+;; Volume 2 of The Art of Computer Programming. 2nd edition.
+
+;; This algorithm evaluates x^n, where n is a positive integer.
+;; (Here x belongs to any algebraic system in which an associative
+;; multiplication, with identity element 1, has been defined.)
+;; A1. [Initialize.] Set N ← n, Y ← 1, Z ← x.
+;; A2. [Halve N.] (At this point, x^n = Y * Z^N.) Set t ← N mod 2 and N ← ⌊N/2⌋.
+;;   If t = 0, skip to step A5.
+;; A3. [Multiply Y by Z.] Set Y ← Z times Y.
+;; A4. [N = 0?] If N = 0, the algorithm terminates, with Y as the answer.
+;; A5. [Square Z.] Set Z ← Z times Z, and return to step A2.
+
+;; Racket manual: https://docs.racket-lang.org/reference/generic-numbers.html
 
 (define (exp x n) (rtl n 1 x))
 (define (rtl N Y Z)
-    (if (even? N)
-        (rtl (quotient N 2) Y (* Z Z))
-            (if (zero? (quotient N 2))
-                (* Z Y)
-                (rtl (quotient N 2) (* Z Y) (* Z Z)))))
+  (let ([t (remainder N 2)]
+        [n (quotient N 2)]
+        [y (* Z Y)]
+        [square-z (* Z Z)])
+    (cond [(zero? N) 1]  ;; 0 is not a positive integer
+          [(zero? t) (rtl n Y square-z)]
+          [(zero? n) y]
+          [else (rtl n y square-z)])))
 
 (exp 3 2)
-; 9
+;; 9
+(exp 3 0)
+;; 1
