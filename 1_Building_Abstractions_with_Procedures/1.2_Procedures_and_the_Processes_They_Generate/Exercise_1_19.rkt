1f74d69a966ae6e386e2fcd75dabcaedfd1fd375
xxyzz <gitpull@protonmail.com>
2020-07-02 17:39:09 +0800

    update 1.19: complete the book's procedure

--- 1_Building_Abstractions_with_Procedures/1.2_Procedures_and_the_Processes_They_Generate/Exercise_1_19.rkt
+++ 1_Building_Abstractions_with_Procedures/1.2_Procedures_and_the_Processes_They_Generate/Exercise_1_19.rkt
@@ -1,24 +1,33 @@
-#lang racket
+#lang racket/base
 
 (define (fib n)
-    (fib-iter 0 1 0 1 n))
+  (fib-iter 1 0 0 1 n))
 
-(define (fib-iter a b x y n)
-    (cond [(zero? n) x]
-          [(even? n)
-            (fib-iter
-                (+ (* a a) (* b b))
-                (+ (* 2 a b) (* b b))
-                x
-                y
-                (/ n 2))]
-          [else
-            (fib-iter
-                a
-                b
-                (+ (* a x) (* b y))
-                (+ (* b x) (* a y) (* b y))
-                (- n 1))]))
+(define (fib-iter a b p q count)
+  (cond [(= count 0) b]
+        [(even? count)
+         (fib-iter a
+                   b
+                   (+ (* q q) (* p p))   ;; compute p′
+                   (+ (* 2 p q) (* q q)) ;; compute q′
+                   (/ count 2))]
+        [else (fib-iter (+ (* b q) (* a q) (* a p))
+                        (+ (* b p) (* a q))
+                        p
+                        q
+                        (- count 1))]))
 
 (fib 3)
-; 2
+;; 2
+
+;; T = | p + q, q |
+;;     | q    , p |
+;;
+;; T * |a| = | bq + aq + ap |
+;;     |b|   | bp + aq      |
+;;
+;; | 1 1 | * |a| = | a + b |
+;; | 1 0 |   |b|   | a     |
+;;
+;; T^2 = | (p + q)^2 + q^2, 2pq + q^2 |
+;;       | 2pq + q^2      , q^2 + p^2 |
