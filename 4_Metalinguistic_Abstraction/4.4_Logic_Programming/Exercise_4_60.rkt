72e7d0f05a3d10f374c86587d9fe711737ab559b
xxyzz <gitpull@protonmail.com>
2020-10-14 21:46:24 +0800

    update 4.60

--- 4_Metalinguistic_Abstraction/4.4_Logic_Programming/Exercise_4_60.rkt
+++ 4_Metalinguistic_Abstraction/4.4_Logic_Programming/Exercise_4_60.rkt
@@ -1,15 +1,22 @@
 #lang racket/base
+(require racket/string) ;; string-append*
 
 ;; different order means different data
 
-(define (name-string person)
-  (string-append (map symbol->string person)))
+(define (name->string person)
+  (string-append* (map symbol->string person)))
 
-(define (compare-name person-1 person-2)
-  (string>? (name-string person-1)
-            (name-string person-2)))
+(define (name>? person-1 person-2)
+  (string>? (name->string person-1)
+            (name->string person-2)))
 
 (rule (lives-near ?person-1 ?person-2)
       (and (address ?person-1 (?town . ?rest-1))
            (address ?person-2 (?town . ?rest-2))
-           (lisp-value compare-name ?person-1 ?person-2)))
+           (lisp-value name>? ?person-1 ?person-2)))
+
+;;; Query results:
+(lives-near (Reasoner Louis) (Aull DeWitt))
+(lives-near (Reasoner Louis) (Bitdiddle Ben))
+(lives-near (Hacker Alyssa P) (Fect Cy D))
+(lives-near (Bitdiddle Ben) (Aull DeWitt))
