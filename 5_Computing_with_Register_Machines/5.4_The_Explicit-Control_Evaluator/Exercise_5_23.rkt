29a7f30db3bd6536a72e215f4c257270b1423fa2
xxyzz <gitpull@protonmail.com>
2020-12-13 10:09:53 +0800

    5.23 pass test

--- 5_Computing_with_Register_Machines/5.4_The_Explicit-Control_Evaluator/Exercise_5_23.rkt
+++ 5_Computing_with_Register_Machines/5.4_The_Explicit-Control_Evaluator/Exercise_5_23.rkt
@@ -17,18 +17,31 @@ eval-dispatch
 (branch (label ev-lambda))
 (test (op begin?) (reg exp))
 (branch (label ev-begin))
-(test (op application?) (reg exp))
-(branch (label ev-application))
 (test (op cond?) (reg exp)) ;; ***
 (branch (label ev-cond))
 (test (op let?) (reg exp)) ;; ***
 (branch (label ev-let))
+(test (op application?) (reg exp))
+(branch (label ev-application))
 (goto (label unknown-expression-type))
 
 ev-cond
 (assign exp (op cond->if) (reg exp)) ;; page 506
-(goto (label ev-if))
+(goto (label eval-dispatch))
 
 ev-let
 (assign exp (op let->combination) (reg exp)) ;; Exercise 4.6
 (goto (label ev-application))
+
+;; tests
+(cond [(= 1 1) 1]
+      [else 2])
+;; 1
+
+(cond [else 2])
+;; 2
+
+(let ([a 1]
+      [b 2])
+  (+ a b))
+;; 3
